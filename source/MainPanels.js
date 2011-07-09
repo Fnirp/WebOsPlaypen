962ae4cb80cc0aa98a4137406d6c5f4cc1d1a9ff
Jenny Ekelund <emailsnooze@gmail.com>
2011-07-09 12:22:26 +0100

    And more git fiddlings
    I've had trouble with deleting and moving files around when committing in git...

---
+++ source/MainPanels.js
@@ -0,0 +1,39 @@
+/**
+ * @author Jenny Ekelund
+ */
+enyo.kind({
+	name: "EngagedBy",
+	kind: enyo.VFlexBox,
+	components: [
+		{name: "slidingPane", kind: "SlidingPane", flex: 1, 
+		  components: [
+			{name: "left", width: "320px", kind:"SlidingView", 
+			  components: [
+        {kind: "Header", content:"Events"},
+        {kind: "Scroller", flex: 1, 
+          components: [   
+          //Insert your components here
+            {kind: "EventsList"}
+					  ]}
+			]},
+			{name: "middle", width: "320px", kind:"SlidingView", peekWidth: 50, components: [
+					{kind: "Header", content:"Recommended"},
+					{kind: "Scroller", flex: 1, components: [
+						//Insert your components here
+					]},
+					{kind: "Toolbar", components: [
+						{kind: "GrabButton"}
+					]}
+			]},
+			{name: "right", kind:"SlidingView", flex: 1, components: [
+					{kind: "Header", content:"Details"},
+					{kind: "Scroller", flex: 1, components: [
+						//Insert your components here
+					]},
+					{kind: "Toolbar", components: [
+						{kind: "GrabButton"}
+					]}
+			]}
+		]}
+	]
+});
