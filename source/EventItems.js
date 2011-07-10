c9205040ce44b80caf34269e746dff07e4ac618a
robmckinnon <rob@theyworkforyou.co.nz>
2011-07-10 12:57:00 +0100

    display event in list item

--- source/EventItems.js
+++ source/EventItems.js
@@ -14,11 +14,11 @@ enyo.kind({
     {kind: "WebService", onSuccess: "queryResponse", onFailure: "queryFail"},
 
     //{kind: "WebService", url: "http://engagedby.com/users/9.json", onSuccess: "queryResponse", onFailure: "queryFail"},
-    
+
     {kind: enyo.Scroller, flex: 1, components: [
       {kind: enyo.DividerDrawer, caption: "Time",components: [
         {kind: enyo.VirtualRepeater, name: "myItemList", onSetupRow: "getEvents", onclick: "selectItem", components: [
-            {kind: enyo.Item, name: "item", layout: enyo.HFlexBox, tapHighlight: true, components: [   
+            {kind: enyo.Item, name: "item", layout: enyo.HFlexBox, tapHighlight: true, components: [
                  {kind: "HFlexBox", components: [
                    {name: "itemName", flex: 1, style: "text-overflow: ellipsis; overflow: hidden; white-space: nowrap;", content: "", flex: 1},
                    {name: "itemDate", style: "text-overflow: ellipsis; overflow: hidden; white-space: nowrap;", content: "1"}
@@ -52,50 +52,49 @@ enyo.kind({
     ]}
 
   ],
-  
+
   console: function(inMessage) {
     this.$.console.setContent(inMessage);
   },
-    
+
   create: function() {
     this.data = [];
     this.inherited(arguments);
   },
 
   loadEventItemsPane: function(url) {
-    console.log("3 user_id: " + url);
     this.console("loadEventItemsPane is called");
     this.console("Url is: " + url);
     this.$.webService.setUrl(url);
     this.$.webService.call();
   },
 
-  
   getEvents: function(inSender, inIndex) {
-    if(this.data.length == 0) return false;
-    var events = this.data[inIndex];
-    
-    if (events) {
-      this.console("Our json string: " + enyo.json.stringify(this.data[inIndex]));
-      var groupId = events.entity.id;
-      var eventName = events.entity.name;
-      var groupImage = events.entity.profile_image_url;
-      this.console("EventName: " + eventName);
+    if(this.data.length === 0) { return false; }
+    var item = this.data[inIndex];
 
-      // bind data to item controls
-  //    this.$.item.applyStyle("background-color", inSender.isSelected(inIndex) ? "lightblue" : null);
+    if (item) {
+      this.console("Our json string: " + enyo.json.stringify(item));
+      console.log("Our json string: " + enyo.json.stringify(item));
+      console.log("Our json string: " + enyo.json.stringify(item.event));
+      var eventName = item.event.name;
+      console.log("Our json string: " + enyo.json.stringify(eventName));
+      var groupId = item.event.entity.id;
+      var groupImage = item.event.entity.profile_image_url;
+
+      //this.$.item.applyStyle("background-color", inSender.isSelected(inIndex) ? "lightblue" : null);
       this.$.itemName.setContent(eventName);
-      this.$.itemDate.setContent("24 May");
+      this.$.itemDate.setContent("");
       this.$.itemOrganisation.applyStyle("font-size", "smaller");
       this.$.itemOrganisation.applyStyle("font-weight", "bold");
-      this.$.itemOrganisation.setContent("My organisation");
+      this.$.itemOrganisation.setContent(item.event.entity.name);
       this.$.itemDescription.applyStyle("color", "grey");
       this.$.itemDescription.applyStyle("font-size", "smaller");
-      this.$.itemDescription.setContent("Some description");
+      this.$.itemDescription.setContent(item.event.start_date_formatted);
       return true;
     }
   },
-  
+
   queryResponse: function(inSender, inResponse) {
     console.log("queryResponse!");
     this.console("EventsItem.queryResponse has been called --------!");
@@ -107,16 +106,16 @@ enyo.kind({
     this.console(JSON.stringify(this.data));
     this.$.myItemList.render();
   },
-  
+
   queryFail: function(inSender, inResponse) {
-    
+
   },
-  
+
   getItemUrl: function() {
      var request_url = "http://engagedby.com/users/" + localStorage.getItem('user_id') + ".json";
      return request_url;
   },
-  
+
   selectItem: function(inSender, inEvent) {
    // this.$.list.select(inEvent.rowIndex); //this doesn't work for whatever reason ....
     var request_url = getItemUrl();
